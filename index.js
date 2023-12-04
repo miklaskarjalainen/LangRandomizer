d3e32eb15d54c30bd52bc2dbca647a8af516e208
Giffi <miklas.karjalainen@gmail.com>
2023-12-04 18:12:17 +0200

    stylistic changes

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ function randomize_clicked() {
     document.getElementById("example-code").innerHTML = highlightedCode;
 }
 
+
+let curX = 0;
+let curY = 0;
+let tgX = 0;
+let tgY = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const Easing = 100; 
+    const InterBubble = document.getElementById("mouse-circle");
+
+    function move() {
+        curX += (tgX - curX) / Easing;
+        curY += (tgY - curY) / Easing;
+        InterBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame( () => {
+            move();
+        })
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    })
+
+    move();
+})
